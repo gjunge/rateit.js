833b0b32ced266dceb0abdb33be65e5086703b4a
Gidon <gjunge@gmail.com>
2016-10-20 15:20:45 +0300

    Add gulp task for minifying Bump version in files

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+
+gulp.task('default', function () {
+    return gulp.src('scripts/jquery.rateit.js')
+       .pipe(sourcemaps.init())
+       .pipe(plumber())
+       .pipe(uglify({ preserveComments: 'license' }))
+       .pipe(rename({ extname: '.min.js' }))
+       .pipe(sourcemaps.write(''))
+       .pipe(gulp.dest('scripts')) // save .min.js
+});
